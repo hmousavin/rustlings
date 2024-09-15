c7a09aa447dc6b4a5595e8edc37f58d8549fc700
hmousavin <hamid.mousavinasab@gmail.com>
2024-09-15 09:00:28 +0330

    ch20

--- exercises/20_threads/threads1.rs
+++ exercises/20_threads/threads1.rs
@@ -13,8 +13,10 @@ fn main() {
     for i in 0..10 {
         let handle = thread::spawn(move || {
             let start = Instant::now();
+            
             thread::sleep(Duration::from_millis(250));
             println!("Thread {i} done");
+            
             start.elapsed().as_millis()
         });
         handles.push(handle);
@@ -22,8 +24,7 @@ fn main() {
 
     let mut results = Vec::new();
     for handle in handles {
-        // TODO: Collect the results of all threads into the `results` vector.
-        // Use the `JoinHandle` struct which is returned by `thread::spawn`.
+        results.push(handle.join().unwrap());
     }
 
     if results.len() != 10 {
@@ -32,6 +33,6 @@ fn main() {
 
     println!();
     for (i, result) in results.into_iter().enumerate() {
-        println!("Thread {i} took {result}ms");
+        println!("Thread {i} took {result} ms");
     }
 }
--- exercises/20_threads/threads3.rs
+++ exercises/20_threads/threads3.rs
@@ -15,12 +15,11 @@ impl Queue {
 }
 
 fn send_tx(q: Queue, tx: mpsc::Sender<u32>) {
-    // TODO: We want to send `tx` to both threads. But currently, it is moved
-    // into the first thread. How could you solve this problem?
+    let cloned_tx = tx.clone();
     thread::spawn(move || {
         for val in q.first_half {
             println!("Sending {val:?}");
-            tx.send(val).unwrap();
+            cloned_tx.send(val).unwrap();
             thread::sleep(Duration::from_millis(250));
         }
     });
